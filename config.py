a08f9c9b7afe76847a524c4e29bd3dfffa206bca
cryptonew <cryptonew2017@gmail.com>
2018-01-06 00:19:01 +0200

    1

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 #config
 ethAddress = "0xb8612b87fb2d3daf374e2723e8c2bbb96f8a1f0c" #NanoPool ETH address to monitor
 iftttKey = "bOFKmLQvh9S_T4Yvvwcx_c" #key from IFTTT Maker applet
-workers_num = 2 # count of your workers
+workers_num = 1 # count of your workers
 
