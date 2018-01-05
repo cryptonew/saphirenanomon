f759a5fc0207a542a92ac90f3b4fcacdaf45a237
Pavlo Bornia <pbornia@lohika.com>
2018-01-06 00:17:08 +0200

    first commit

---
+++ config.py
@@ -0,0 +1,5 @@
+#config
+ethAddress = "0xb8612b87fb2d3daf374e2723e8c2bbb96f8a1f0c" #NanoPool ETH address to monitor
+iftttKey = "bOFKmLQvh9S_T4Yvvwcx_c" #key from IFTTT Maker applet
+workers_num = 2 # count of your workers
+
